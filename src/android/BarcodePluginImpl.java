93b9b86d3d4d56515d03bf3fe3e50044f78ea0f9
Maximilian Weiler <maximilian.weiler@sap.com>
2020-02-17 15:04:06 +0100

    Fix scanning when multiple instances are running - Release scanner and EMDK on pause of the app - Initialize EMDK and the scanner on resume

--- src/android/BarcodePluginImpl.java
+++ src/android/BarcodePluginImpl.java
@@ -10,34 +10,27 @@ import android.util.Log;
 
 import s4h.retail.store.zebra.barcode.EMDKBridge;
 
-public class BarcodePluginImpl extends CordovaPlugin  {
+public class BarcodePluginImpl extends CordovaPlugin {
 	public static final String LOG_TAG = "s4h.retail.store.zebra.barcode.BarcodePluginImpl";
 	private static final String CORDOVA_ACTION_START = "Start";
 	private static final String CORDOVA_ACTION_STOP = "Stop";
 	private CordovaWebView webView = null;
 	private EMDKBridge emdkBridge = null;
-	
+
 	@Override
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 		super.initialize(cordova, webView);
 		this.webView = webView;
-		this.emdkBridge = new EMDKBridge(
-			(scannedBarcode) -> { sendScan(scannedBarcode); },
-			() -> { return cordova.getActivity().getApplicationContext(); });
-			try {
-			emdkBridge.initializeEmdk();
-		} catch (NoClassDefFoundError e) {
-			Log.e(LOG_TAG,"EMDK not available. Please make sure to provide the Zebra EMDK library.");
-		} catch(RuntimeException re) {
-			//Unfortunately, the exception class on non-zebra devices is only RuntimeException and the text only stub
-			Log.e(LOG_TAG,"Unexpected Runtime Exception (" + re.getClass().getName() + ") -- " + re.getMessage());
-			Log.e(LOG_TAG,"Could not initialize RFM Zebra Barcode (are you using a non Zebra-Device ?)");
-		}
+		this.emdkBridge = new EMDKBridge((scannedBarcode) -> {
+			sendScan(scannedBarcode);
+		}, () -> {
+			return cordova.getActivity().getApplicationContext();
+		});
 	}
 
 	/**
-	 * Send the scanned barcode as event to the UI thread
-	 * so that the Fiori UIs can handle it.
+	 * Send the scanned barcode as event to the UI thread so that the Fiori UIs can
+	 * handle it.
 	 */
 	private void sendScan(final String barcode) {
 		cordova.getActivity().runOnUiThread(() -> {
@@ -69,12 +62,22 @@ public class BarcodePluginImpl extends CordovaPlugin  {
 	public void onPause(boolean multitasking) {
 		super.onPause(multitasking);
 		emdkBridge.stopScan();
+		emdkBridge.shutdown();
 	}
 
 	@Override
 	public void onResume(boolean multitasking) {
 		super.onResume(multitasking);
-		emdkBridge.startScan();
+		try {
+			emdkBridge.initializeEmdk();
+		} catch (NoClassDefFoundError e) {
+			Log.e(LOG_TAG, "EMDK not available. Please make sure to provide the Zebra EMDK library.");
+		} catch (RuntimeException re) {
+			// Unfortunately, the exception class on non-zebra devices is only
+			// RuntimeException and the text only stub
+			Log.e(LOG_TAG, "Unexpected Runtime Exception (" + re.getClass().getName() + ") -- " + re.getMessage());
+			Log.e(LOG_TAG, "Could not initialize RFM Zebra Barcode (are you using a non Zebra-Device ?)");
+		}
 	}
 
 }
